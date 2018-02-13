6970b4bd5b8074c6d063bda889a831e2fc9bbd32
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2018-02-13 14:47:46 +0200

    Added initial support for semantic segmentation labels of NYUDepth (just depths OR segmentation, not both yet). Removed standardization for now, must recompute mean/std and use getMean/getStd methods

--- src/readers/nyudepth_reader.py
+++ src/readers/nyudepth_reader.py
@@ -4,23 +4,27 @@ from .dataset_reader import DatasetReader
 from transforms import Transformer
 
 class NYUDepthReader(DatasetReader):
-	def __init__(self, datasetPath, labels="depths", imagesShape=(240, 320, 3), depthShape=(55, 74), \
+	def __init__(self, datasetPath, labelsType="depths", imagesShape=(240, 320, 3), labelShape=(55, 74), \
 		normalization="normalize", transforms=[], dataSplit=(60, 30, 10)):
-		assert len(imagesShape) == 3 and len(depthShape) == 2
-		assert labels == "depths", "Only depths supported for now. Expected values 'depths', 'labels', 'both'"
+		assert len(imagesShape) == 3 and len(labelShape) == 2
+		assert labelsType in ("depths", "segmentation"), "Only \"depths\" and \"segmentation\" supported for now. " + \
+			"Expected values \"depths\", \"segmentation\", \"both\""
 		assert normalization in ("normalize", "standardize")
 		self.datasetPath = datasetPath
 		self.imagesShape = imagesShape
-		self.depthShape = depthShape
+		self.labelShape = labelShape
 		self.normalization = normalization
 		self.transforms = transforms
 		self.dataSplit = dataSplit
-		self.dataAugmenter = Transformer(transforms, dataShape=imagesShape, labelShape=depthShape, \
+		self.labelsType = labelsType
+		self.dataAugmenter = Transformer(transforms, dataShape=imagesShape, labelShape=labelShape, \
 			applyOnDataShapeForLabels=True)
 		self.setup()
 
 	def setup(self):
 		# Check validity of the dataSplit (sums to 100 and positive)
+		# TODO: maybe move some of this in a base class, since it's going to be common code anyway (getNumIterations
+		#  expects self.numData for example, so it's tied code).
 		assert len(self.dataSplit) == 3 and self.dataSplit[0] >= 0 and self.dataSplit[1] >= 0 \
 			and self.dataSplit[2] >= 0 and self.dataSplit[0] + self.dataSplit[1] + self.dataSplit[2] == 100
 
@@ -52,24 +56,27 @@ class NYUDepthReader(DatasetReader):
 			assert startIndex < endIndex, "startIndex < endIndex. Got values: %d %d" % (startIndex, endIndex)
 			numData = endIndex - startIndex
 
+			labelType = "depths" if self.labels == depths else "labels"
 			# N x 3 x 640 x 480 => N x 480 x 640 x 3
 			images = np.swapaxes(self.dataset["images"][startIndex : endIndex], 1, 3).astype(np.float32)
-			# N x 640 x 480 => N x 480 x 640
-			depths = np.swapaxes(self.dataset["depths"][startIndex : endIndex], 1, 2)
+			# N x 640 x 480 => N x 480 x 640. Labels can be "depths" or "labels" (for segmentation), not both yet.
+			labels = np.swapaxes(self.dataset[labelType][startIndex : endIndex], 1, 2)
 
 			if self.normalization == "standardize":
-				img_mean = np.array([122.54539034, 104.78338964, 100.02394636])
-				img_std = np.array([73.74140829, 75.4556151, 78.87249644])
+				assert False, "Standardization not yet supported"
+				# img_mean = np.array([122.54539034, 104.78338964, 100.02394636])
+				# img_std = np.array([73.74140829, 75.4556151, 78.87249644])
 
-				images -= img_mean
-				images /= img_std
+				# images -= img_mean
+				# images /= img_std
 			# Perhaps a better name ?
 			elif self.normalization == "normalize":
 				images /= 255
-				depths /= 10
+				if self.labelsType == "depths":
+					labels /= 10
 
 			# Use the "none" transform just for its resize operation it does at end.
-			yield self.dataAugmenter.applyTransform("none", images, depths)
+			yield self.dataAugmenter.applyTransform("none", images, labels)
 			# Apply each transform
-			for augImages, augDepths in self.dataAugmenter.applyTransforms(images, depths):
-				yield augImages, augDepths
+			for augImages, augLabels in self.dataAugmenter.applyTransforms(images, labels):
+				yield augImages, augLabels
