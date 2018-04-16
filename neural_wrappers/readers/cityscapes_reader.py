32329b4fd85b56709e4dd6fc34b0adc585f5ea24
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2018-04-16 19:33:03 +0300

    Removed CityScapes video reader (will come with a general video reader sub-class when I will need it based on this code, but for now it's obsolete). Renamed CityScapesImagesReader to CityScapesReader

--- neural_wrappers/readers/cityscapes_reader.py
+++ neural_wrappers/readers/cityscapes_reader.py
@@ -4,11 +4,10 @@ from .dataset_reader import DatasetReader
 from neural_wrappers.transforms import Transformer
 from neural_wrappers.utilities import standardizeData
 
-# A secondary reader for CityScapes algorithm. This one instead of taking raw videos, expects a pre-compiuted h5py
-#  file from which it loads the data.
-class CityScapesImagesReader(DatasetReader):
+# CityScapes Reader class, used with the data already converted in h5py format.
+class CityScapesReader(DatasetReader):
 	def __init__(self, datasetPath, imageShape, labelShape, transforms=["none"], flowAlgorithm=None):
-		assert flowAlgorithm is None or flowAlgorithm in ("flownet2")
+		assert flowAlgorithm is None or flowAlgorithm in ("flownet2s")
 		self.datasetPath = datasetPath
 		self.imageShape = imageShape
 		self.labelShape = labelShape
@@ -21,32 +20,19 @@ class CityScapesImagesReader(DatasetReader):
 
 	def setup(self):
 		self.dataset = h5py.File(self.datasetPath, "r")
-		self.numData = {Type : len(self.dataset[Type]["images"]) for Type in ("test", "train", "validation")}
+		self.numData = {Type : len(self.dataset[Type]["seq_rgb_5"]) for Type in ("test", "train", "validation")}
 
-		# These values are for cityscapes_v1
-		# self.means = {
-		# 	"images" : [72.0077217043444, 81.15868796015802, 70.88520810466197],
-		# 	"depths" : 80.00175220926421,
-		# 	"flownet2" : [-0.046412684, 2.4327936]
-		# }
-
-		# self.stds = {
-		# 	"images" : [48.06690873921935, 48.75020861009473, 48.214420108835114],
-		# 	"depths" : 64.32797789334958,
-		# 	"flownet2" : [32.09688, 11.09397]
-		# }
-
-		# These values are for cityscapes_v2
+		# These values are for cityscapes V2 and above (not images from video frames).
 		self.means = {
-			"images" : [74.96715607296854, 84.3387139353354, 73.62945761147961],
-			"depths" : 8277.619363028218,
-			"flownet2" : [-0.6396361, 5.553444]
+			"rgb" : [74.96715607296854, 84.3387139353354, 73.62945761147961],
+			"depth" : 8277.619363028218,
+			"flownet2s" : [-0.6396361, 5.553444]
 		}
 
 		self.stds = {
-			"images" : [49.65527668307159, 50.01892939272212, 49.67332749250472],
-			"depths" : 6569.138224069467,
-			"flownet2" : [32.508713, 15.168872]
+			"rgb" : [49.65527668307159, 50.01892939272212, 49.67332749250472],
+			"depth" : 6569.138224069467,
+			"flownet2s" : [32.508713, 15.168872]
 		}
 
 		print(("[CityScapes Images Reader] Setup complete. Num data: Train: %d, Test: %d, Validation: %d. " + \
@@ -64,8 +50,8 @@ class CityScapesImagesReader(DatasetReader):
 			endIndex = min((i + 1) * miniBatchSize, self.numData[type])
 			assert startIndex < endIndex, "startIndex < endIndex. Got values: %d %d" % (startIndex, endIndex)
 
-			images = thisData["images"][startIndex : endIndex]
-			depths = thisData["depths"][startIndex : endIndex]
+			images = thisData["seq_rgb_5"][startIndex : endIndex]
+			depths = thisData["seq_depth_5"][startIndex : endIndex]
 
 			if not self.flowAlgorithm is None:
 				flow = thisData[self.flowAlgorithm][startIndex : endIndex]
@@ -78,10 +64,10 @@ class CityScapesImagesReader(DatasetReader):
 				yield augImages, augDepths
 
 	def normalizeData(self, images, depths):
-		images[..., 0] = standardizeData(images[..., 0], self.means["images"][0], self.stds["images"][0])
-		images[..., 1] = standardizeData(images[..., 1], self.means["images"][1], self.stds["images"][1])
-		images[..., 2] = standardizeData(images[..., 2], self.means["images"][2], self.stds["images"][2])
-		depths = standardizeData(depths, self.means["depths"], self.stds["depths"])
+		images[..., 0] = standardizeData(images[..., 0], self.means["rgb"][0], self.stds["rgb"][0])
+		images[..., 1] = standardizeData(images[..., 1], self.means["rgb"][1], self.stds["rgb"][1])
+		images[..., 2] = standardizeData(images[..., 2], self.means["rgb"][2], self.stds["rgb"][2])
+		depths = standardizeData(depths, self.means["depth"], self.stds["depth"])
 		if not self.flowAlgorithm is None:
 			images[..., 3] = standardizeData(images[..., 3], self.means[self.flowAlgorithm][0], \
 				self.stds[self.flowAlgorithm][0])
