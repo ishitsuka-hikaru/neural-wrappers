76b438497c8ec15b4e8e6e1db486675519a32092
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2018-06-06 21:01:46 +0300

    Added IndoorCVPR09Reader

--- neural_wrappers/readers/__init__.py
+++ neural_wrappers/readers/__init__.py
@@ -8,7 +8,8 @@ from .kitti_reader import KITTIReader
 from .corrupter_reader import CorrupterReader
 from .combined_dataset_reader import CombinedDatasetReader
 from .kitti_obj_reader import KITTIObjReader
+from .indoor_cvpr09_reader import IndoorCVPR09Reader
 
 __all__ = ["DatasetReader", "ClassificationDatasetReader", "NYUDepthReader", "CitySimReader", "MNISTReader", \
 	"CityScapesReader", "Cifar10Reader", "KITTIReader", "CorrupterReader", "CombinedDatasetReader", \
-	"KITTIObjReader"]
+	"KITTIObjReader", "IndoorCVPR09Reader"]
