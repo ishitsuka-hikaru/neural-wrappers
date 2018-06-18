7780aa81f5a48f15eb9e8b18563b614afed7ea12
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2018-06-18 15:38:33 +0300

    Added support for NYUDepth semantic training. Added semantic num classes (defaults to 49). Depcrecated NYUDepthV1, only V2 is supported

--- neural_wrappers/readers/nyudepth_reader.py
+++ neural_wrappers/readers/nyudepth_reader.py
@@ -3,18 +3,19 @@ import numpy as np
 from .dataset_reader import DatasetReader
 from neural_wrappers.transforms import Transformer
 
+# NYUDepthV2 only
 class NYUDepthReader(DatasetReader):
 	def __init__(self, datasetPath, imageShape, labelShape, transforms=["none"], \
 		normalization="min_max_normalization", dataDimensions=["images"], labelDimensions=["depths"], \
-		dataSplit=(80, 0, 20), version="v2"):
+		dataSplit=(80, 0, 20), semanticNumClasses=50, semanticTransform="none"):
 		if "rgb" in dataDimensions:
 			dataDimensions[dataDimensions.index("rgb")] = "images"
 
 		super().__init__(datasetPath, imageShape, labelShape, dataDimensions, labelDimensions, \
 			transforms, normalization)
-		assert version in ("v1", "v2")
-		self.version = version
 		self.dataSplit = dataSplit
+		self.semanticNumClasses = semanticNumClasses
+		self.semanticTransform = semanticTransform
 		self.setup()
 
 	def __str__(self):
@@ -28,6 +29,16 @@ class NYUDepthReader(DatasetReader):
 	# 		z[np.where(y == b[i])] = i
 	# 	return z
 
+	def semanticNewDims(self, images):
+		numClasses = len(self.topClasses)
+		newImages = np.zeros((numClasses, *images.shape), dtype=np.float32)
+		for i in range(numClasses):
+			thisOne = newImages[i]
+			whereId = np.where(images == self.topClasses[i])
+			thisOne[whereId] = 1
+		newImages = np.transpose(newImages, [1, 3, 2, 0])
+		return newImages
+
 	def setup(self):
 		self.dataset = h5py.File(self.datasetPath, "r")
 		self.supportedDimensions = ["images", "depths", "labels"]
@@ -35,49 +46,55 @@ class NYUDepthReader(DatasetReader):
 		self.indexes, self.numData = self.computeIndexesSplit(numAllData)
 
 		# Top 49 classes for each version of the dataset. The 50th is considered "other" and has value of 0.
-		if self.version == "v1":
-			topClasses = np.array([1296, 99, 964, 423, 81, 1122, 137, 182, 341, 216, 296, 249, 823, 46, 188, 1027, \
-				1362, 1360, 67, 357, 1160, 70, 450, 898, 685, 1084, 113, 942, 19, 827, 990, 1210, 683, 1060, 1314, \
-				606, 557, 7, 996, 893, 872, 691, 462, 732, 291, 285, 730, 1225, 106])
-		elif self.version == "v2":
-			topClasses = np.array([21, 11, 3, 157, 5, 83, 28, 19, 59, 64, 88, 80, 7, 4, 36, 89, 42, 122, 169, 119, \
+		assert self.semanticNumClasses <= 49 and self.semanticNumClasses >= 1
+		topClasses = np.array([21, 11, 3, 157, 5, 83, 28, 19, 59, 64, 88, 80, 7, 4, 36, 89, 42, 122, 169, 119, \
 				143, 141, 85, 17, 172, 15, 123, 135, 26, 45, 331, 158, 144, 24, 124, 136, 55, 2, 238, 8, 1, 312, 94, \
 				49, 242, 12, 87, 161, 298])
+		self.topClasses = topClasses[0 : self.semanticNumClasses]
+
+		self.postDataProcessing = {
+			"images" : lambda x : np.swapaxes(x, 1, 3),
+			"depths" : lambda x : np.swapaxes(x, 1, 2)
+		}
+
+		semanticNumDims = 1
+		if "labels" in self.dataDimensions:
+			assert self.semanticTransform in ("none", "semantic_new_dims")
+			if self.semanticTransform == "none":
+				prepareSemantic = lambda x : np.expand_dims(np.swapaxes(x, 1, 2), axis=-1)
+			elif self.semanticTransform == "semantic_new_dims":
+				semanticNumDims = self.semanticNumClasses
+				prepareSemantic = self.semanticNewDims
+			self.postDataProcessing["labels"] = prepareSemantic
 
 		self.minimums = {
 			"images" : [0, 0, 0],
 			"depths" : 0,
-			"labels" : 0
+			"labels" : [0] * semanticNumDims if semanticNumDims > 1 else 0
 		}
 
 		self.maximums = {
 			"images" : [255, 255, 255],
 			"depths" : 10,
-			"labels" : 1
+			"labels" : [1] * semanticNumDims if semanticNumDims > 1 else 1
 		}
 
 		self.means = {
 			"images" : [122.54539034418492, 104.78338963563233, 100.02394636162512],
 			"depths" : 2.7963083,
-			"labels" : 0
+			"labels" : [0] * semanticNumDims if semanticNumDims > 1 else 0
 		}
 
 		self.stds = {
 			"images" : [73.74140829480243, 75.45561510079736, 78.87249644483357],
 			"depths" : 1.3860533,
-			"labels" : 1
+			"labels" : [1] * semanticNumDims if semanticNumDims > 1 else 1
 		}
 
 		self.numDimensions = {
 			"images" : 3,
 			"depths" : 1,
-			"labels" : 1
-		}
-
-		self.postDataProcessing = {
-			"images" : lambda x : np.swapaxes(x, 1, 3),
-			"depths" : lambda x : np.swapaxes(x, 1, 2),
-			"labels" : lambda x : np.expand_dims(np.swapaxes(x, 1, 2), axis=-1)
+			"labels" : semanticNumDims
 		}
 
 		self.postSetup()
