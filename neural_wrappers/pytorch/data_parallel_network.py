5c81c225e55c91185651bb2fc4148dc09b890dd2
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2019-04-05 16:25:12 +0300

    Potential fix for DataParallel v2

---
+++ neural_wrappers/pytorch/data_parallel_network.py
@@ -0,0 +1,16 @@
+from .network import NeuralNetworkPyTorch
+import torch.nn as nn
+
+# Hackish solution so we can use NeuralNetworkPyTorch's methods (run_one_epoch, train_generator etc.) while still
+#  using pytorch's DataParallel module, which scatters and gathers data in multiple devices automagically.
+class DataParallelNNetwork(NeuralNetworkPyTorch):
+	def __init__(self, model):
+		super().__init__()
+		self.baseModel = nn.DataParallel(model)
+
+	def forward(self, *args, **kwargs):
+		return self.baseModel.forward(*args, **kwargs)
+
+	def __str__(self):
+		return "General parallel neural network architecture. Update __str__ in your model for more details when " + \
+			"using summary."
