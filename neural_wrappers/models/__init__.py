7ae51565b93f9110b48f1c7e5791806d93dd8f19
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2019-10-19 02:14:18 +0300

    Added Model SafeUAV TinySum model (used for SafeUAV paper)

--- neural_wrappers/models/__init__.py
+++ neural_wrappers/models/__init__.py
@@ -2,6 +2,7 @@ from .model_eigen import ModelEigen
 from .model_laina import ModelLaina
 from .model_unet import ModelUNet
 from .model_unet_dilatedconv import ModelUNetDilatedConv
+from .model_safeuav_tinysum import ModelSafeUAVTinySum
 from .upsample import UpSampleLayer
 from .identity import IdentityLayer
 from .model_mobilenetv2_cifar10 import MobileNetV2Cifar10
