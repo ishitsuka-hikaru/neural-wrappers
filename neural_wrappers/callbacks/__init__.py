61cd175f87c827b12b4e2e489c89236a7f20eb78
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2019-11-05 16:00:45 +0200

    Compatibility fix for PlotMetricsCallback import

--- neural_wrappers/callbacks/__init__.py
+++ neural_wrappers/callbacks/__init__.py
@@ -4,4 +4,4 @@ from .metric_as_callback import MetricAsCallback
 from .save_models import SaveModels
 from .save_history import SaveHistory
 from .confusion_matrix import ConfusionMatrix
-from .plot_metrics import PlotMetrics
+from .plot_metrics import PlotMetrics, PlotMetrics as PlotMetricsCallback
