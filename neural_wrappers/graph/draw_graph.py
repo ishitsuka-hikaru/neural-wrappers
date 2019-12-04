10c3ba467ea7daba49bf34e60df66e027d2eb2e6
Pîrvu Mihai Cristian <mihaicristianpirvu@gmail.com>
2019-12-04 18:38:37 +0200

    Slightly updated the draw graph method

--- neural_wrappers/graph/draw_graph.py
+++ neural_wrappers/graph/draw_graph.py
@@ -21,30 +21,30 @@ class GraphDrawer:
 	# TODO: edge-edge is only supported (Add node-edge, edge-node, node-node edge types)
 	def draw(self, fileName, cleanup):
 		# Create the graph using the original labels
-		dot = Digraph(format="png", engine="dot")
+		dot = Digraph(format="png", engine="fdp")
 
 		# Each node also has a subgraph for other stuff, like GT box or edge networks
 		subgraphs = {}
 		for node in self.nodes:
 			dot.node(name=self.mapNodes[node], label=node.name, shape="oval")
-			subgraphs[node] = Digraph(engine="neato")
+			subgraphs[node] = Digraph()
 
 		# Add basic edges
 		for i in range(len(self.edges)):
 			edge = self.edges[i]
 			A, B = edge.inputNode, edge.outputNode
-			dot.edge(self.mapNodes[A], self.mapNodes[B], len="2.0", label="%d" % (i + 1))
+			dot.edge(self.mapNodes[A], self.mapNodes[B], len="2.0", label="  %d  " % (i + 1))
 
 		# Add GT edges
 		for node in self.nodes:
 			if not node.groundTruthKey:
 				continue
-			subgraphs[node].node(name="cluster_GT-%s" % (self.mapNodes[node]), label="GT", shape="box")
-			subgraphs[node].edge("cluster_GT-%s" % (self.mapNodes[node]), self.mapNodes[node])
+			subgraphs[node].node(name="cluster_GT-%s" % (self.mapNodes[node]), label="GT", shape="box", width="0.3")
+			subgraphs[node].edge("cluster_GT-%s" % (self.mapNodes[node]), self.mapNodes[node], len="0.5", orientation="90")
 
 		for node in self.nodes:
 			dot.subgraph(subgraphs[node])
-		dot.render(fileName, view=True, cleanup=cleanup)
+		dot.render(fileName, view=False, cleanup=cleanup)
 
 def drawGraph(nodes, edges, fileName, cleanup):
 	GraphDrawer(list(nodes), list(edges)).draw(fileName, cleanup)
