77d0021332bd5da5ae638af5f5bddf441714984d
Your Name <you@example.com>
2019-12-24 16:22:28 +0200

    Updated plot metrics to new MetricAsCallback api (including direction)

--- neural_wrappers/callbacks/plot_metrics.py
+++ neural_wrappers/callbacks/plot_metrics.py
@@ -2,20 +2,16 @@ from .callback import Callback
 from ..pytorch.pytorch_utils import plotModelMetricHistory
 
 class PlotMetrics(Callback):
-	def __init__(self, metrics, plotBestBullet=None, dpi=120, **kwargs):
+	def __init__(self, metrics, **kwargs):
 		super().__init__(**kwargs)
 		assert len(metrics) > 0, "Expected a list of at least one metric which will be plotted."
-		self.metrics = metrics
-		self.dpi = dpi
-		self.plotBestBullet = plotBestBullet
-		if self.plotBestBullet == None:
-			self.plotBestBullet = ["none"] * len(self.metrics)
+		self.metrics = list(metrics.keys())
 
 	def onEpochEnd(self, **kwargs):
 		trainHistory = kwargs["trainHistory"]
 		if not kwargs["isTraining"] or len(trainHistory) == 1:
 			return
 
-		for i, metric in enumerate(self.metrics):
-			bullet = self.plotBestBullet[i]
-			plotModelMetricHistory(metric, trainHistory, bullet)
+		for metric in self.metrics:
+			direction = kwargs["model"].getMetrics()[metric].getDirection()
+			plotModelMetricHistory(metric, trainHistory, direction)
