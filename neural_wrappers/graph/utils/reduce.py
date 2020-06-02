6894cd25973d72cde4eb9e116aecc4d403e6ccc8
Mihai Cristian Pirvu <mihaicristianpirvu@gmail.com>
2020-06-02 23:47:07 +0300

    Updated reduce node further to include node's metrics and loss.

--- neural_wrappers/graph/utils/reduce.py
+++ neural_wrappers/graph/utils/reduce.py
@@ -12,20 +12,10 @@ class ReduceNode(Edge):
 		self.inputs = x
 		res = self.forwardFn(self, x)
 		self.outputs = res
+		# Clear node's messages and replace them with the reduced version.
 		self.inputNode.messages = {}
 		self.inputNode.addMessage(self, res)
 		return self.outputs
-
-	def loss(self, y, t):
-		return None
-
-	def setupModel(self):
-		assert self.model is None
-		self.model = trModuleWrapper(lambda x : x)
-		self.lossFn = lambda y, t : None
-
-	def getMetrics(self):
-		return {}
 
 	def getDecoder(self):
 		return trModuleWrapper(lambda x : x)
