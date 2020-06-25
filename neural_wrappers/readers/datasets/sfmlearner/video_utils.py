abe3553962cadd4536368d394cae60cdf1bd9765
Meehai <mihaicristianpirvu@gmail.com>
2020-06-25 10:30:13 +0300

    Fix for random split in video utils

--- neural_wrappers/readers/datasets/sfmlearner/video_utils.py
+++ neural_wrappers/readers/datasets/sfmlearner/video_utils.py
@@ -1,37 +1,40 @@
 import numpy as np
+from typing import Dict
 
-# Indices functions
-def computeIndicesRandom(indices, sequenceSize, skipFrames):
-	seqIndices = computeIndicesSequential(indices, sequenceSize, skipFrames)
-	np.random.seed(42)
-	perm = np.random.permutation(len(seqIndices))
-	return seqIndices[perm]
-
-def computeIndicesSequential(indices, sequenceSize, skipFrames):
-	l, r = sequenceSize // 2 - (sequenceSize % 2 == 0), sequenceSize // 2
-	a = indices[0]
-	leftRightRange = np.arange(-l * skipFrames, (r * skipFrames) + 1, skipFrames)
-	seqIndices = np.array([leftRightRange + x for x in indices], dtype=np.int32)
-	return seqIndices
-
-# Split functions
-def getRandomSplits(videoLength, dataSplits, sequenceSize, skipFrames):
-	np.random.seed(42)
-	permutation = np.random.permutation(videoLength)
-	sequentialSplits = getSequentialSplits(videoLength, dataSplits)
-	randomSplits = {k : permutation[sequentialSplits[k]] for k in dataSplits}
-	return randomSplits
-
-def getVideoExtremes(videoLength, sequenceSize, skipFrames):
+# Utility functions
+def getVideoExtremes(videoLength:int, sequenceSize:int, skipFrames:int):
 	# left: avoid these indices that would get me to negative counts for the biggest first index given a sequence
 	#  size and skipFrames. So S=5, skipFrames=2 means that for the first item, the center needs to be in [4].
 	#  Becuase the first item has indexes: [0, 2, 4, 6, 8]
 	l = ((sequenceSize // 2 - (sequenceSize % 2 == 0))) * skipFrames
 	r = videoLength - ((sequenceSize // 2) * skipFrames) - 1
-	validVideoLength = (r - l)
 	return l, r
 
-def getSequentialSplits(videoLength, dataSplits, sequenceSize, skipFrames):
+# Indices functions
+def computeIndicesRandom(indices:np.ndarray, sequenceSize:int, skipFrames:int):
+	seqIndices = computeIndicesSequential(indices, sequenceSize, skipFrames)
+	np.random.seed(42)
+	perm = np.random.permutation(len(seqIndices))
+	return seqIndices[perm]
+
+def computeIndicesSequential(indices:np.ndarray, sequenceSize:int, skipFrames:int):
+	l, r = sequenceSize // 2 - (sequenceSize % 2 == 0), sequenceSize // 2
+	leftRightRange = np.arange(-l * skipFrames, (r * skipFrames) + 1, skipFrames)
+	seqIndices = np.array([leftRightRange + x for x in indices], dtype=np.int32)
+	return seqIndices
+
+# Split functions
+def getRandomSplits(videoLength:int, dataSplits:Dict[str, float], sequenceSize:int, skipFrames:int):
+	sequentialSplits = getSequentialSplits(videoLength, dataSplits, sequenceSize, skipFrames)
+
+	randomSplits = {}
+	np.random.seed(42)
+	for k in sequentialSplits:
+		perm = np.random.permutation(len(sequentialSplits[k]))
+		randomSplits[k] = sequentialSplits[k][perm]
+	return randomSplits
+
+def getSequentialSplits(videoLength:int, dataSplits:Dict[str, float], sequenceSize:int, skipFrames:int):
 	l, r = getVideoExtremes(videoLength, sequenceSize, skipFrames)
 	validVideoLength = r - l
 	startIndex = l
@@ -45,10 +48,9 @@ def getSequentialSplits(videoLength, dataSplits, sequenceSize, skipFrames):
 	indices[k] = (indices[k][0], r)
 	return {k : np.array(range(indices[k][0], indices[k][1]), dtype=np.int32) for k in dataSplits}
 
-def computeIndices(dataSplitMode, dataSplits, videoLength, sequenceSize, skipFrames):
+def computeIndices(dataSplitMode:str, dataSplits:Dict[str, float], videoLength:int, sequenceSize:int, skipFrames:int):
 	assert dataSplitMode in ("random", "sequential", "sequential_then_random", "random_no_overlap")
 	assert sequenceSize < videoLength - 2, "Sequence size: %d. Len video: %d" % (sequenceSize, videoLength)
-	l, r = getVideoExtremes(videoLength, sequenceSize, skipFrames)
 
 	# This is a two process step. First, we need to divide the data given the data splits. This takes the pressure
 	#  off of the algorithms to do this step as well as compute their logic. The result is the middle frame of the
