faa5b2dadb6ea64fe2ec221dbb7030e2a3803976
Mihai Cristian Pîrvu <mihaicristianpirvu@gmail.com>
2020-12-19 12:47:50 +0200

    Fucked up some merge.

--- test/readers/test_batched_dataset_reader.py
+++ test/readers/test_batched_dataset_reader.py
@@ -59,7 +59,7 @@ class TestBatchedDatasetReader:
 		for j, (batchItem, B) in enumerate(reader.iterateForever()):
 			try:
 				assert B == batchSizes[j % n]
-			except Exception:ptest_
+			except Exception:
 				breakpoint()
 			rgb = batchItem["data"]["rgb"]
 			index = reader.getIndex(j % n)
@@ -68,43 +68,8 @@ class TestBatchedDatasetReader:
 			if j == 100:
 				break
 
-	def test_splitItems_1(self):
-		reader = Reader(DummyDataset())
-		rgbMerged = reader.dataset[0 : 2]
-		itemMerged = {"data" : {"rgb" : rgbMerged}, "labels" : {"class" : [0, 0]}}
-		item1, item2 = reader.splitItems(itemMerged, 1)
-		rgb1 = item1["data"]["rgb"]
-		rgb2 = item2["data"]["rgb"]
-		assert np.abs(rgb1 - rgbMerged[0]).sum() < 1e-5
-		assert np.abs(rgb2 - rgbMerged[1]).sum() < 1e-5
-
-	def test_mergeSplit_1(self):
-		reader = Reader(DummyDataset())
-		item1, B1 = reader.getItem(0)
-		item2, B2 = reader.getItem(1)
-		rgb1 = item1["data"]["rgb"]
-		rgb2 = item2["data"]["rgb"]
-
-		for i in range(3):
-			itemMerged = reader.mergeItems(item1, B1, item2, B2)
-			item1_split, item2_split = reader.splitItems(itemMerged, 1)
-			rgb1_split = item1_split["data"]["rgb"]
-			rgb2_split = item2_split["data"]["rgb"]
-			assert np.abs(rgb1 - rgb1_split).sum() < 1e-5
-			assert np.abs(rgb2 - rgb2_split).sum() < 1e-5
-
-			item1 = {"data" : {"rgb" : rgb1_split}, "labels" : {"class" : [0]}}
-			item2 = {"data" : {"rgb" : rgb2_split}, "labels" : {"class" : [0]}}
-
 def main():
-<<<<<<< HEAD
 	TestBatchedDatasetReader().test_constructor_1()
-=======
-	TestDatasetReader().test_constructor_1()
-	TestDatasetReader().test_mergeItems_1()
-	TestDatasetReader().test_splitItems_1()
-	TestDatasetReader().test_mergeSplit_1()
->>>>>>> 034f641e2534ddbb31c385d7d55489a7107a665b
 
 if __name__ == "__main__":
 	main()
