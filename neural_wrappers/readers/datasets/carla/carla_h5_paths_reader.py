2882b732c86adf0d07d1be1abc5ae4dddcdd5305
Mihai Cristian Pîrvu <mihaicristianpirvu@gmail.com>
2020-12-24 14:10:34 +0200

    Fix for pose. Working ngc-time with old models

--- neural_wrappers/readers/datasets/carla/carla_h5_paths_reader.py
+++ neural_wrappers/readers/datasets/carla/carla_h5_paths_reader.py
@@ -8,15 +8,16 @@ from ....utilities import tryReadNpy
 def prevReader(dataset:h5py._hl.group.Group, index, dimGetter, neighbours, delta) -> np.ndarray:
 	assert delta == -1
 	Key = "t-1"
-	prevIndex = neighbours[Key][index.start : index.stop]
+	prevIndex = neighbours[Key][index]
 	return dimGetter(dataset, prevIndex)
 
 def opticalFlowReader(dataset:h5py._hl.group.Group, index, neighbours, delta:int) -> np.ndarray:
 	baseDirectory = str(dataset.file["others"]["baseDirectory"][()], "utf8")
 	assert delta == -1
 	Key = "t-1"
-	prevIndex = neighbours[Key][index]
 	flowKey = "optical_flow(%s, t)" % Key
+
+	prevIndex = neighbours[Key][index]
 	paths = np.array([dataset[flowKey][ix] for ix in prevIndex])
 	paths_x, paths_y = paths[:, 0], paths[:, 1]
 	paths_x = ["%s/%s" % (baseDirectory, str(x, "utf8")) for x in paths_x]
@@ -37,31 +38,31 @@ class CarlaH5PathsReader(H5BatchedDatasetReader):
 		flowReadFunction = opticalFlowReader
 
 		dimGetter = {
-			"rgb":partial(pathsReader, readFunction=rawReadFunction, dim="rgb"),
-			"depth":partial(pathsReader, readFunction=depthReadFunction, dim="depth"),
-			"pose":partial(defaultH5DimGetter, dim="position"),
-			"semantic_segmentation":partial(pathsReader, readFunction=rawReadFunction, \
+			"rgb" : partial(pathsReader, readFunction=rawReadFunction, dim="rgb"),
+			"depth" : partial(pathsReader, readFunction=depthReadFunction, dim="depth"),
+			"pose" : partial(defaultH5DimGetter, dim="position"),
+			"semantic_segmentation" : partial(pathsReader, readFunction=rawReadFunction, \
 				dim="semantic_segmentation"),
-			"wireframe":partial(pathsReader, readFunction=rawReadFunction, dim="wireframe"),
-			"wireframe_regression":partial(pathsReader, readFunction=rawReadFunction, dim="wireframe"),
-			"halftone":partial(pathsReader, readFunction=rawReadFunction, dim="halftone"),
-			"normal":partial(pathsReader, readFunction=rawReadFunction, dim="normal"),
-			"cameranormal":partial(pathsReader, readFunction=rawReadFunction, dim="cameranormal"),
-			"rgbDomain2":partial(pathsReader, readFunction=rawReadFunction, dim="rgbDomain2"),
+			"wireframe" : partial(pathsReader, readFunction=rawReadFunction, dim="wireframe"),
+			"wireframe_regression" : partial(pathsReader, readFunction=rawReadFunction, dim="wireframe"),
+			"halftone" : partial(pathsReader, readFunction=rawReadFunction, dim="halftone"),
+			"normal" : partial(pathsReader, readFunction=rawReadFunction, dim="normal"),
+			"cameranormal" : partial(pathsReader, readFunction=rawReadFunction, dim="cameranormal"),
+			"rgbDomain2" : partial(pathsReader, readFunction=rawReadFunction, dim="rgbDomain2"),
 		}
 
 		dimTransform ={
 			"data":{
-				"rgb":partial(rgbNorm, readerObj=self),
-				"depth":partial(depthNorm, readerObj=self),
-				"pose":partial(poseNorm, readerObj=self),
-				"semantic_segmentation":partial(semanticSegmentationNorm, readerObj=self),
-				"wireframe":partial(wireframeNorm, readerObj=self),
-				"wireframe_regression":partial(wireframeRegressionNorm, readerObj=self),
-				"halftone":partial(halftoneNorm, readerObj=self),
-				"normal":partial(normalNorm, readerObj=self),
-				"cameranormal":partial(normalNorm, readerObj=self),
-				"rgbDomain2":partial(rgbNorm, readerObj=self),
+				"rgb" : partial(rgbNorm, readerObj=self),
+				"depth" : partial(depthNorm, readerObj=self),
+				"pose" : partial(poseNorm, readerObj=self),
+				"semantic_segmentation" : partial(semanticSegmentationNorm, readerObj=self),
+				"wireframe" : partial(wireframeNorm, readerObj=self),
+				"wireframe_regression" : partial(wireframeRegressionNorm, readerObj=self),
+				"halftone" : partial(halftoneNorm, readerObj=self),
+				"normal" : partial(normalNorm, readerObj=self),
+				"cameranormal" : partial(normalNorm, readerObj=self),
+				"rgbDomain2" : partial(rgbNorm, readerObj=self),
 			}
 		}
 
@@ -70,8 +71,10 @@ class CarlaH5PathsReader(H5BatchedDatasetReader):
 		neighbours = {"t-1" : self.getIdAtTimeDelta(ids, delta=-1)}
 		dimGetter["optical_flow(t-1, t)"] = partial(opticalFlowReader, neighbours=neighbours, delta=-1)
 		dimTransform["data"]["optical_flow(t-1, t)"] = partial(opticalFlowNorm, readerObj=self)
+		dimGetter["pose(t-1)"] = partial(prevReader, dimGetter=dimGetter["pose"], neighbours=neighbours, delta=-1)
+		dimTransform["data"]["pose(t-1)"] = dimTransform["data"]["pose"]
 		# Add all (t-1) items
-		for key in ["rgb", "depth", "wireframe_regression", "pose", "semantic_segmentation", "normal", \
+		for key in ["rgb", "depth", "wireframe_regression", "semantic_segmentation", "normal", \
 			"cameranormal", "halftone"]:
 			dimGetter["%s(t-1)" % key] = partial(prevReader, dimGetter=dimGetter[key], \
 				neighbours=neighbours, delta=-1)
@@ -80,10 +83,6 @@ class CarlaH5PathsReader(H5BatchedDatasetReader):
 		super().__init__(datasetPath, dataBuckets, dimGetter, dimTransform)
 		self.hyperParameters = hyperParameters
 		self.desiredShape = desiredShape
-
-		# self.rawReadFunction = rawReadFunction
-		# self.numNeighboursAhead = numNeighboursAhead
-		# self.idOfNeighbour = {"t-1" : self.getIdAtTimeDelta(delta=-1)}
 
 	# For each top level (train/tet/val) create a new array with the index of the frame at time t + skipFrames.
 	# For example result["train"][0] = 2550 means that, after randomization the frame at time=1 is at id 2550.
