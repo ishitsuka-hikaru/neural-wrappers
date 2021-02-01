70bbabb7849085f65b618a804675da4e933ee758
Mihai Cristian Pirvu <mihaicristianpirvu@gmail.com>
2021-02-01 08:16:23 +0200

    Updated code to be truly random by adding a plotIterationFn callback to decide the iteration at epoch start

--- neural_wrappers/callbacks/random_plot_each_epoch.py
+++ neural_wrappers/callbacks/random_plot_each_epoch.py
@@ -6,37 +6,54 @@ from .callback import Callback
 from ..utilities import changeDirectory
 
 _plotFn = None
+_plotIterationFn = None
 
 class RandomPlotEachEpoch(Callback):
-	def __init__(self, plotFn:Callable, baseDir:str="samples"):
+	# @param[in] Plot function that receives the 3 arguments (x, y, t) for that particular iteration
+	# @param[in] baeseDir The subdirectory where the samples are created. By default it's '$(pwd)/samples/'
+	# @param[in] plotIterationFn Callback to get the iteration at which the main callback is called.
+	#  By default it's 0 (start of epoch).
+	def __init__(self, plotFn:Callable, baseDir:str="samples", plotIterationFn=lambda : 0):
 		super().__init__(name="RandomPlotEachEpoch (Dir='%s')" % baseDir)
 		self.baseDir = baseDir
 		self.plotFn = plotFn
-		global _plotFn
+		self.plotIterationFn = plotIterationFn
+		self.currentEpoch = None
+		self.plotIteration = None
+
+		global _plotFn, _plotIteration
 		_plotFn = plotFn
+		_plotIterationFn = plotIterationFn
 
 	@overrides
 	def onEpochStart(self, **kwargs):
-		if kwargs["isTraining"]:
-			Dir = "%s/%d" % (self.baseDir, kwargs["epoch"])
-		else:
-			Dir = "%s/test" % self.baseDir
-		Path(Dir).mkdir(exist_ok=True, parents=True)
 		self.currentEpoch = kwargs["epoch"]
-		self.epochDir = Dir
+		self.plotIteration = self.plotIterationFn()
 
 	@overrides
 	def onIterationEnd(self, results, labels, **kwargs):
-		# if kwargs["iteration"] == 0 and kwargs["isOptimizing"] == False:
-		if kwargs["iteration"] == 0:
-			cwd = os.path.realpath(os.path.abspath(os.curdir))
-			os.chdir(self.epochDir)
-			self.plotFn(kwargs["data"], results, labels)
-			os.chdir(cwd)
+		if kwargs["iteration"] != self.plotIteration:
+			return
+
+		if kwargs["isTraining"]:
+			if kwargs["isOptimizing"]:
+				Dir = "%s/%d/train" % (self.baseDir, self.currentEpoch)
+			else:
+				Dir = "%s/%d/validation" % (self.baseDir, self.currentEpoch)
+		else:
+			Dir = "%s/test" % self.baseDir
+		Path(Dir).mkdir(exist_ok=True, parents=True)
+
+		cwd = os.path.realpath(os.path.abspath(os.curdir))
+		os.chdir(Dir)
+		self.plotFn(kwargs["data"], results, labels)
+		os.chdir(cwd)
 
 	def onCallbackLoad(self, additional, **kwargs):
-		global _plotFn
+		global _plotFn, _plotIterationFn
 		self.plotFn = _plotFn
+		self.plotIterationFn = _plotIterationFn
 
 	def onCallbackSave(self, **kwargs):
 		self.plotFn = None
+		self.plotIterationFn = None
