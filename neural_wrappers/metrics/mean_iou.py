33635bf38440ca4a6406aa86d991dfb31371c932
Mihai Cristian Pirvu <mihaicristianpirvu@gmail.com>
2021-02-05 15:05:54 +0200

    small fix

--- neural_wrappers/metrics/mean_iou.py
+++ neural_wrappers/metrics/mean_iou.py
@@ -63,7 +63,8 @@ class GlobalMeanIoU(Metric):
 			cU = union[..., i].sum()
 			self.intersections[i] += cI
 			self.unions[i] += cU
-		return self.fReturn()
+		# return self.fReturn()
+		return np.array([0])
 
 class MeanIoU(Metric):
 	def __init__(self, mode="local", returnMean=True):
